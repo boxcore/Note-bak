e103a6aa2e3920eb472b0b4c87bd4ad7fccbb0f7
thunje@gmail.com <thunje@gmail.com@ce4becd2-237d-d507-14b9-f7b526dc84f6>
2013-12-28 05:43:53 +0000

    git-svn-id: https://boxcore.googlecode.com/svn/trunk@50 ce4becd2-237d-d507-14b9-f7b526dc84f6

---
+++ syn/phpstorm设置.js
@@ -0,0 +1,5 @@
+// phpstorm 设置
+设置自定义字体 http://superuser.com/questions/586142/webstorm-phpstorm-select-specific-font-from-a-font-family
+
+
+// 参考网站
---
+++ syn/work/Code/批量替换正则.js
@@ -0,0 +1,4 @@
+// 2013 12 27 
+
+批量查找函数：
+href="<\?php\s+echo\s+(?!src\_url)(?!site\_url)
