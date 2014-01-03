396eb5ee3206a20269e39819d6f4931698925a4c
thunje@gmail.com <thunje@gmail.com@ce4becd2-237d-d507-14b9-f7b526dc84f6>
2014-01-03 15:12:38 +0000

    git-svn-id: https://boxcore.googlecode.com/svn/trunk@57 ce4becd2-237d-d507-14b9-f7b526dc84f6

---
+++ syn/cn-server/src/down.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget http://mirrors.sohu.com/mysql/MySQL-5.5/mysql-5.5.35-linux2.6-x86_64.tar.gz
+wget http://mirrors.sohu.com/php/php-5.3.9.tar.gz
+wget http://mirrors.sohu.com/nginx/nginx-1.5.8.tar.gz
+wget http://www.cmake.org/files/v2.8/cmake-2.8.12.tar.gz
