edd9d37cae9a1821667075817fae00f7c8e01d44
thunje@gmail.com <thunje@gmail.com@ce4becd2-237d-d507-14b9-f7b526dc84f6>
2014-02-12 06:06:56 +0000

    提交msd数据库管理文件
    git-svn-id: https://boxcore.googlecode.com/svn/trunk@82 ce4becd2-237d-d507-14b9-f7b526dc84f6

--- note/codeigniter/ci中COOKIE操作.js
+++ note/codeigniter/ci中COOKIE操作.js
@@ -1,12 +1,12 @@
 CI中常用COOKIE操作
 
-//第一种设置cookie的方式：采用php原生态的方法设置的cookie的值
+// 第一种设置cookie的方式：采用php原生态的方法设置的cookie的值
 setcookie("user_id",$user_info['user_id'],86500);
 setcookie("username",$user_info['username'],86500);
 setcookie("password",$user_info['password'],86500);
-//echo $_COOKIE['username'];
+// echo $_COOKIE['username'];
 
-//第二种设置cookie的方式：通过CI框架的input类库设置cookie的值
+// 第二种设置cookie的方式：通过CI框架的input类库设置cookie的值
 $this->input->set_cookie("username",$user_info['username'],60);
 $this->input->set_cookie("password",$user_info['password'],60);
 $this->input->set_cookie("user_id",$user_info['user_id'],60);
@@ -15,7 +15,7 @@ $this->input->set_cookie("user_id",$user_info['user_id'],60);
 //echo $_COOKIE['username'];//在模型类中可以通过这种方式获取cookie值
 //echo $_COOKIE['password'];//在模型类中可以通过这种方式获取cookie值
 
-//第三种设置cookie的方式：通过CI框架的cookie_helper.php辅助函数库设置cookie的值
+// 第三种设置cookie的方式：通过CI框架的cookie_helper.php辅助函数库设置cookie的值
 set_cookie("username",$user_info['username'],60);
 set_cookie("password",$user_info['password'],60);
 set_cookie("user_id",$user_info['user_id'],60);
